92a5c2025a00d96edf00cedf717d88bf0fb38c11
Hristiyan_Yanev <h.yanev@dssolutions.bg>
2021-12-28 20:29:04 +0200

    add view model

---
+++ backend/demo/src/main/java/javaspring/demo/domain/models/view/AllCarsCarViewModel.java
@@ -0,0 +1,39 @@
+package javaspring.demo.domain.models.view;
+
+import java.math.BigDecimal;
+
+public class AllCarsCarViewModel {
+    private String id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private AllCarsCarViewModel(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
