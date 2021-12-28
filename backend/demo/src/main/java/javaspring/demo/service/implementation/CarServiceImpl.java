db87d33af9a2f8279d0f76234d55ef61752dffe9
Hristiyan_Yanev <h.yanev@dssolutions.bg>
2021-12-28 20:30:37 +0200

    add service impl

---
+++ backend/demo/src/main/java/javaspring/demo/service/implementation/CarServiceImpl.java
@@ -0,0 +1,69 @@
+package javaspring.demo.service.implementation;
+
+import javaspring.demo.domain.entities.Car;
+import javaspring.demo.domain.models.service.CarServiceModel;
+import javaspring.demo.repository.CarRepository;
+import javaspring.demo.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final CarRepository carRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public CarServiceImpl(CarRepository carRepository, ModelMapper modelMapper){
+        this.carRepository = carRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public boolean createCar(CarServiceModel carServiceModel) {
+        Car carEntity = this.modelMapper.map(carServiceModel, Car.class);
+
+        try{
+            this.carRepository.save(carEntity);
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Set<CarServiceModel> getTop4CarsByViews() {
+        return this.carRepository.findTop4ByOrderByViews()
+                .stream()
+                .map(x -> modelMapper.map(x, CarServiceModel.class))
+                .collect(Collectors.toCollection(
+                        LinkedHashSet::new
+                ));
+    }
+
+    @Override
+    public Set<CarServiceModel> allCars() {
+        return this
+                .carRepository
+                .findAll()
+                .stream()
+                .map(x -> modelMapper
+                .map(x, CarServiceModel.class))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public CarServiceModel getCarById(String id) {
+        Car carEntity = this.carRepository.findById(id).orElse(null);
+
+        if (carEntity == null) return null;
+
+        return this.modelMapper.map(carEntity, CarServiceModel.class);
+    }
+}
